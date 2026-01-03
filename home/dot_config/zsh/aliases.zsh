85a241f6a8994dde99ed8ea6a5a6c71da9a32fa7
wiizzl <git@houlliere.com>
2026-01-03 20:59:18 +0100

    fix: reorganize zsh configuration; remove old alias and profile files, and consolidate settings into new templates

--- home/dot_config/zsh/aliases.zsh
+++ home/dot_config/zsh/aliases.zsh
@@ -1,6 +1,8 @@
 alias \
 \
 c="clear" \
+ping="ping -c 5" \
+please="sudo" \
 cat="bat" \
 nano="micro" \
 ls="eza -l --icons --group-directories-first --git" \
@@ -9,7 +11,3 @@ ll="eza --icons -a --group-directories-first -1 --no-user --long" \
 tree="eza --icons --tree --group-directories-first" \
 -g -- -h="-h 2>&1 | bat --language=help --style=plain" \
 -g -- --help="--help 2>&1 | bat --language=help --style=plain"
-
-if [[ "$TERM" == "xterm-kitty" ]]; then
-  alias ssh="kitten ssh"
-fi
