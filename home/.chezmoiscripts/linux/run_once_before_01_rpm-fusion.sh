69ec2bdfa69f68b08e18bd6bddde9ff8880384e4
Pierre H. <git@houlliere.com>
2026-03-01 16:36:26 +0100

    fix: adjust indentation for consistency in configuration templates

--- home/.chezmoiscripts/linux/run_once_before_01_rpm-fusion.sh
+++ home/.chezmoiscripts/linux/run_once_before_01_rpm-fusion.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 sudo dnf install -y \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
