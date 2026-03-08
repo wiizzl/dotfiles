128cdc6f6a26cc7b67899e0f2d659b4a73f7d2f2
Pierre H. <git@houlliere.com>
2026-03-08 11:16:35 +0100

    fix: remove vesktop

--- home/.chezmoiscripts/linux/run_once_before_01_repos.sh
+++ home/.chezmoiscripts/linux/run_once_before_01_repos.sh
@@ -7,14 +7,24 @@ sudo dnf install -y \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+# Terra
+# sudo dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+
 # Flathub
-flatpak remote-delete fedora
+flatpak remote-delete --force fedora
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Copr
-sudo dnf copr enable jdxcode/mise
-sudo dnf copr enable quadratech188/vicinae
+sudo dnf copr enable -y jdxcode/mise quadratech188/vicinae
+sudo dnf copr enable -y quadratech188/vicinae
 
 # VSCode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo tee /etc/yum.repos.d/vscode.repo > /dev/null <<EOF
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
--- home/.chezmoiscripts/linux/run_once_before_03_codecs.sh
+++ home/.chezmoiscripts/linux/run_once_before_03_codecs.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
-sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf update -y @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
