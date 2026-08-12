e6fe2feb55f373f84274996c182efd6b475db4bd
Pierre Houllière <git@houlliere.com>
2026-08-12 15:28:40 +0200

    feat: auto open herdr

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -3,6 +3,11 @@ if status is-interactive
     starship init fish | source
     zoxide init fish --cmd cd | source
     fzf --fish | source
+
+    if not set -q IN_HERDR; and type -q herdr
+        set -gx IN_HERDR 1
+        herdr
+    end
 end
 
 fish_add_path $HOME/.local/bin
