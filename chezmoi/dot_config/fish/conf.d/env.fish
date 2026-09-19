b0900dbfe500d43d42d2ddfcf15f04353cc5d16b
Pierre Houllière <git@houlliere.com>
2026-09-19 19:09:47 +0200

    fix: starship config

--- chezmoi/dot_config/fish/conf.d/env.fish
+++ chezmoi/dot_config/fish/conf.d/env.fish
@@ -1,6 +1,6 @@
 set -g fish_greeting ""
 
 set -gx MANPAGER "bat -plman"
-set -gx STARSHIP_CONFIG "$HOME/.config/starship.toml"
+set -gx STARSHIP_CONFIG "$HOME/.config/starship/config.toml"
 
 set -gx VISUAL "zed --wait"
